cc8b4f26467e391acc315ca5cd83fda3dd04a346
RTSJ Linux Local User <rtsj@pcl4105c.esiee.fr>
2017-11-20 10:09:52 +0100

    Add start project files

---
+++ src/tej/controller/Tache_Compteur.java
@@ -0,0 +1,37 @@
+package tej.controller;
+
+import javax.realtime.*; 
+
+public class Tache_Compteur extends RealtimeThread
+{ 
+ 
+  int compteur = 0;
+  int cycle = 0;
+  
+  public Tache_Compteur(SchedulingParameters sched, ReleaseParameters release)
+  {
+    super(sched, release); 
+  }
+  
+  public void run()
+  {
+	  
+	  System.out.println("debut de la tache");
+	  while(true)
+	  {
+		  
+		  
+		  // A COMPLETER...
+		  
+		  if(++cycle>25){
+			  	System.out.println("Temps trop grand");
+			  	break;
+		  }
+		 
+		  
+	  }
+		  
+	  System.out.println("fin de la tache");
+  } 
+}
+
